259e1cda59a306491d4c393a28980fbfb4ae08b8
justinjames92 <nj9428947@gmail.com>
2024-05-13 10:29:02 -0400

    sidebar created , logo added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Row, Col } from "react-bootstrap";
 import { AppProvider } from "./AppContext";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <AppProvider>
         <Row>
           <Col md={2} className="sidebar">
+            <Sidebar/>
           </Col>
           <Col md={10} className="taskManager">
           </Col>
